623df6e90725fcaeb2b23107173ac5fad0ab5fdb
Xin Wang <imwangxin@gmail.com>
2015-06-15 21:50:34 -0700

    changed fuel density

--- mcnp/change_fuel_density.py
+++ mcnp/change_fuel_density.py
@@ -1,7 +1,26 @@
-import os
-with f = open('fhr', 'w+'):
-	lines = f.readlines()
-	starting = 18
-	print lines(18,13:23) 
-        for i in range(0, 160):
-		pass
+import fileinput
+import sys
+import string
+
+with open('fhr', 'r+') as f:
+    lines = f.readlines()
+    print len(lines)
+    lnb = 17
+    ncg =0
+    starting = lines[17]
+    print starting[12:23]
+    for z in range(0, 5):
+        for r in range(0, 4):
+            for i in range(0, 8):
+                line = lines[lnb]
+                print 'line %d change %r to -10.5' %(lnb, line[12:23])
+                line = string.replace(line, line[12:23], '-10.50', 1)
+                print line
+                lines[lnb] = line
+                ncg +=1
+                lnb = lnb + 5
+            lnb = lnb+ 2
+        # lnb = lnb + 1
+        print ncg
+with open('fhr_fuel_density', 'w+') as of:
+    of.write(''.join(lines))
